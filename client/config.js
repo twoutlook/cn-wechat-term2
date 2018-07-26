f4436ebb825b785e89210af4eb42e6cc893c24c9
Mark Chen <twcloudwebapp@outlook.com>
2018-07-26 23:07:40 +0800

    extend 4-3

--- client/config.js
+++ client/config.js
@@ -3,7 +3,10 @@
  */
 
 // 此处主机域名修改成腾讯云解决方案分配的域名
-var host = 'https://hzhpfdkv.qcloud.la/';
+// var host = 'https://hzhpfdkv.qcloud.la/'; 
+
+// by Mark 7/27 22:47
+var host = 'https://untbxjpi.qcloud.la'
 
 var config = {
 
